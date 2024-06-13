1f7c32b68e6b93ba364634b65cb70e511d5cc1ae
Cheattos <rrqlemin@gmail.com>
2024-06-13 12:35:14 +0700

    Done g bang?

---
+++ Day-12/index.js
@@ -0,0 +1,116 @@
+const express = require("express");
+// ini ada di node js
+const path = require("path");
+
+const app = express();
+const port = 5000;
+
+const data = [];
+
+// app.set
+// mendeskripsikan templte engine apa yang dipake
+app.set("view engine", "hbs");
+// ini memberitahu si templte engine ngambilnya dari folder mana
+app.set("views", path.join(__dirname, "src/views"));
+
+// ini untuk assets
+app.use("/assests", express.static(path.join(__dirname, "src/assests")));
+
+// middleware -> yang berfungsi sebagai alat memproses inputan dari form (Request)
+app.use(express.urlencoded({ extended: false }));
+
+// Routing
+app.get("/", home);
+
+app.get("/blog", blog);
+app.get("/add-blog", viewblog);
+app.post("/add-blog", addBlog);
+app.get("/blog-detail/:id", blogDetail);
+app.get("/update-blog/:id", editBlogView);
+app.post("/update-blog", updateBlog);
+app.post("/delete-blog/:id", deleteBlog);
+
+app.get("/testimonial", testimonial);
+app.get("/contact", contactme);
+
+function home(req, res) {
+  res.render("index");
+}
+
+// Blog
+function blog(req, res) {
+  res.render("blog", { data });
+}
+
+function viewblog(req, res) {
+  res.render("add-blog");
+}
+
+function addBlog(req, res) {
+  const { title, content } = req.body;
+
+  console.log("Title :", title);
+  console.log("content :", content);
+
+  const dataBlog = { title, content };
+
+  const uwu = data.unshift(dataBlog);
+
+  console.log(uwu);
+
+  res.redirect("blog");
+}
+
+function blogDetail(req, res) {
+  const { id } = req.params;
+
+  const detail = data[id];
+
+  res.render("blog-detail", { detail });
+}
+
+function editBlogView(req, res) {
+  const { id } = req.params;
+
+  // kenapa kita ubah id menjadi integer walaupun idnya sudah berupa angka
+  // karan biasanya pada real project id ini tipernya adalah UUID (campuran angka dan huruf yg digenerate otomatis )
+  // kalo disini kita pakenya angka aja dulu, sebagai contoh referensi NANTI
+
+  const datafilter = data[parseInt(id)];
+  datafilter.id = parseInt(id);
+  res.render("update-blog", { data: datafilter });
+}
+
+function updateBlog(req, res) {
+  const { title, content, id } = req.body;
+
+  data[parseInt(id)] = {
+    title,
+    content,
+  };
+
+  res.redirect("/blog");
+}
+
+function deleteBlog(req, res) {
+  const { id } = req.params;
+
+  console.log("data sebelum:", data);
+  data.splice(id, 1);
+  console.log("data sesudah:", data);
+
+  res.redirect("/blog");
+}
+// Blog
+
+function testimonial(req, res) {
+  res.render("testimonial");
+}
+
+function contactme(req, res) {
+  res.render("contact");
+}
+
+app.listen(port, () => {
+  console.log(`Server berjalan pada port ${port}`);
+});
---
+++ Day-12/src/assests/js/testimonials.js
@@ -0,0 +1,118 @@
+// Promise
+//  Promise adalah sebuah object yang mempresentasikan dari operasi asyncronus yang memiliki sebuah
+// kondisi yang harus dipenuhi  atau menghasilkan nilai.
+// 1. kondisi yang harus dipenuhi
+// 2. operasi yang belum selesai
+
+// Promise punya 3 kondisi
+// 1. Pending (tunggu/tunda) = Ketika dia sedang dijalankan / dipanggil (undifinded)
+// 2. Fullfilled (Terselesaikan) = Ketika janjinya terlaksana / terpenuhi / berhasil (value)
+// 3. Rejected (Gagal/Tertolak) = Ketika janjinya tidak terlaksana / di jalankan (error)
+
+// API
+// Appication Programming Interface (API)
+// API ini sebuah data / kumpulan data yang sudah ada / tersedia dari aplikasi lain/server,
+// yang dapat kita gunakan/pakai untuk dikelola kembali
+
+const testimonial = new Promise((resolve, reject) => {
+  const xhr = new XMLHttpRequest();
+  // Apa itu xml?
+  //  Ini sebuah fungsi yang memungkinkan kita untuk / dapat memanggil sebuah API.
+  // Yang kita ambil adalah sebuah Request HTTP dari server yang akan dikirim ke kita
+  // berupa sebuah response.
+  // xhr ini merupakan salah satu cara asyncronus untuk memanggil request HTTP
+
+  xhr.open("GET", "https://api.npoint.io/b3a4c7ed44162a465ce0", true);
+  // Get -> Methode
+  // yg-2 -> Alamat api
+  // kondisi 3 -> sebuah izin
+
+  // Get -> untuk mendapatkan API
+
+  // Post -> Ini untuk mengirimkan data ke API
+  // PUT -> untuk merubah data di API
+  // Delete -> menghapus data di API
+
+  // fungsi true -> fungsinya jadi asyncronus (dijalankan secara bersamaan)
+  // fungsi false -> fungsinya jadi syncronus (dijalankan secara berurutan)
+
+  xhr.onload = function () {
+    if (xhr.status == 200) {
+      resolve(JSON.parse(xhr.response));
+    } else {
+      reject("Erorr Loaded Data");
+      // salah menuliskan alamat
+      // gangguan langsung dari server (lagi perbaikan)
+    }
+  };
+
+  xhr.onerror = function () {
+    reject("404 Not Found");
+    // CORS
+  };
+
+  // Mengirim permintaan ke HTTP ke Server
+  xhr.send();
+});
+
+async function showTestimonial() {
+  try {
+    const response = await testimonial;
+    // await disini sebagai penanda bahwa butuh waktu tunggu samapi data dari API kita dapatkan.
+    // setiap penggunaan async sudah pasti pake await
+    // Hasil dari pemanggilan jika tidak menggunakan await maka akan diberikan output undifinded
+    // Jika pake await akan tampil datanya
+    let testimonialHtml = ``;
+
+    response.forEach((item) => {
+      testimonialHtml += `
+      <div class="col-md-6 col-lg-4 mb-4">
+                <div class="card shadow-sm">
+                    <img src="${item.image}"
+                         alt="testimonial" class="card-img-top">
+                    <div class="card-body">
+                        <p class="card-text">${item.content}</p>
+                        <p class="card-text text-end font-weight-bold">- ${item.author}</p>
+                        <p class="card-text text-end font-weight-bold">${item.rating} <i class="fa-solid fa-star"></i></p>
+                    </div>
+                </div>
+            </div>`;
+    });
+
+    document.getElementById("testimonials").innerHTML = testimonialHtml;
+  } catch (error) {
+    console.log(error);
+  }
+}
+// callback -> pemanggilan fungsi kembali
+showTestimonial();
+
+async function filterTestimonials(rating) {
+  try {
+    const response = await testimonial;
+    let testimonialHtml = ``;
+
+    const dataFilter = response.filter((data) => data.rating === rating);
+    if (dataFilter.length === 0) {
+      testimonialHtml = `<h1> Data not found!</h1>`;
+    } else {
+      dataFilter.forEach((item) => {
+        testimonialHtml += `<div class="col-md-6 col-lg-4 mb-4">
+        <div class="card shadow-sm">
+            <img src="${item.image}"
+                 alt="testimonial" class="card-img-top">
+            <div class="card-body">
+                <p class="card-text">${item.content}</p>
+                <p class="card-text text-end font-weight-bold">- ${item.author}</p>
+                <p class="card-text text-end font-weight-bold">${item.rating} <i class="fa-solid fa-star"></i></p>
+            </div>
+        </div>
+    </div>`;
+      });
+    }
+
+    document.getElementById("testimonials").innerHTML = testimonialHtml;
+  } catch (error) {
+    console.log(error);
+  }
+}
